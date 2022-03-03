7cde7c9898f1017338ee856c96fc5a19d90f57a2
jaspinderkaur <jaspinder.kaur028@gmail.com>
2022-03-03 12:35:15 +0400

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'chatroom#index'
-  get 'login', to: 'sessions#new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
