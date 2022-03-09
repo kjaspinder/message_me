287186368e47d06bc141de1d7d48446ee22f5166
jaspinderkaur <jaspinder.kaur028@gmail.com>
2022-03-09 12:15:56 +0400

    add messages partial

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+User.create(username: "Sachin", password: "password")
+User.create(username: "Jyoti", password: "password")
+User.create(username: "Juhi", password: "password")
+User.create(username: "Mom", password: "password")
+User.create(username: "Dad", password: "password")
